e9a13611cd876fbf9552cede8a49f5ecf2151809
Dan <danielmi@unity3d.com>
2020-04-27 23:27:36 -0700

    project cleanup, fixed prefab and robot assets, stub script for UI management

---
+++ Assets/BodyRecording/Scripts/BodyRecordingUIManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class BodyRecordingUIManager : MonoBehaviour
+{
+    [SerializeField]
+    ARHumanBodyManager m_HumanBodyManager;
+
+    [SerializeField]
+    GameObject m_BodyRecordingUI;
+
+    [SerializeField]
+    GameObject m_BodyPlacingUI;
+
+    void OnEnable()
+    {
+        m_HumanBodyManager.humanBodiesChanged += HumanBodyManagerOnhumanHumanBodiesChanged;
+    }
+
+    void OnDisable()
+    {
+        m_HumanBodyManager.humanBodiesChanged -= HumanBodyManagerOnhumanHumanBodiesChanged;
+    }
+
+    void HumanBodyManagerOnhumanHumanBodiesChanged(ARHumanBodiesChangedEventArgs obj)
+    {
+        
+    }
+}
