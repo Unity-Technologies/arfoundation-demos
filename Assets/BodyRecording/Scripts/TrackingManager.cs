67a47862d2820bbc2e42113faaa439f7062058b6
Dan <danielmi@unity3d.com>
2020-04-28 00:00:31 -0700

    testing out switching tracking states at runtime, from body tracking to world tracking

---
+++ Assets/BodyRecording/Scripts/TrackingManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class TrackingManager : MonoBehaviour
+{
+    public enum CurrentTrackingState
+    {
+        BodyTracking,
+        WorldTracking
+    }
+
+    public CurrentTrackingState TrackingState;
+
+    [SerializeField]
+    ARSession m_Session;
+
+    [SerializeField]
+    ARSessionOrigin m_SessionOrigin;
+
+    [SerializeField]
+    ARPlaneManager m_PlaneManager;
+
+    [SerializeField]
+    ARPointCloudManager m_PointCloudManager;
+
+    [SerializeField]
+    ARRaycastManager m_RaycastManager;
+
+    [SerializeField]
+    ARHumanBodyManager m_HumanBodyManager;
+
+    [SerializeField]
+    PlaceObjectsOnPlane m_PlaceObjectsOnPlane;
+
+    public void SwitchToWorldTracking()
+    {
+        SwitchTracking(CurrentTrackingState.WorldTracking);
+    }
+    
+    
+    void SwitchTracking(CurrentTrackingState newTrackingState)
+    {
+        if (newTrackingState == CurrentTrackingState.BodyTracking)
+        {
+            
+        }
+        else
+        {
+            // clean up and remove tracked bodies
+            m_HumanBodyManager.SetTrackablesActive(false);
+            m_HumanBodyManager.enabled = false;
+            
+            // enable subsystems for world tracking
+            m_PlaneManager.enabled = true;
+            m_PointCloudManager.enabled = true;
+            m_RaycastManager.enabled = true;
+            m_PlaceObjectsOnPlane.enabled = true;
+        }
+    }
+}
