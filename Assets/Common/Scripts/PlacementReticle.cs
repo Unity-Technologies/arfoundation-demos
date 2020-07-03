aff4b864f53af08da47d60d744ee76860271fc62
Dan <danielmi@unity3d.com>
2020-07-02 23:08:31 -0700

    wip meshing demo, reticle for placement, Object handler

---
+++ Assets/Common/Scripts/PlacementReticle.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementReticle : MonoBehaviour
+{
+    [SerializeField]
+    bool m_SnapToMesh;
+
+    [SerializeField]
+    ARRaycastManager m_RaycastManager;
+
+    [SerializeField]
+    GameObject m_ReticlePrefab;
+
+    GameObject m_SpawnedReticle;
+    CenterScreenHelper m_CenterScreen;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    TrackableType m_RaycastMask;
+
+    void OnEnable()
+    {
+        m_CenterScreen = CenterScreenHelper.Instance;
+        if (m_SnapToMesh)
+        {
+            m_RaycastMask = TrackableType.PlaneEstimated;
+        }
+        else
+        {
+            m_RaycastMask = TrackableType.PlaneWithinPolygon;
+        }
+
+        m_SpawnedReticle = Instantiate(m_ReticlePrefab);
+        m_SpawnedReticle.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (m_RaycastManager)
+        {
+            if (m_RaycastManager.Raycast(m_CenterScreen.GetCenterScreen(), s_Hits, m_RaycastMask))
+            {
+                Pose hitPose = s_Hits[0].pose;
+                m_SpawnedReticle.transform.SetPositionAndRotation(hitPose.position, hitPose.rotation);
+                m_SpawnedReticle.SetActive(true);
+            }
+            else
+            {
+                m_SpawnedReticle.SetActive(false);
+            }
+        }
+    }
+
+    public Transform GetReticlePosition()
+    {
+        // if not active ie: not snapping to a plane return null
+        if (!m_SpawnedReticle.activeSelf)
+        {
+            return null;
+        }
+        else
+        {
+            return m_SpawnedReticle.transform;
+        }
+    }
+}
---
+++ Assets/Meshing/Scripts/ClassificationPlacementManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassificationPlacementManager : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> m_FloorPrefabs;
+
+    [SerializeField]
+    List<GameObject> m_TablePrefabs;
+
+    [SerializeField]
+    List<GameObject> m_WallPrefabs;
+
+    [SerializeField]
+    MeshClassificationManager m_ClassificationManager;
+    
+    
+}
