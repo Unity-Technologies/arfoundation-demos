cb7398b7e5440046b5abe6502d6e776c810faf4a
Dan <danielmi@unity3d.com>
2020-07-16 19:59:51 -0700

    added UI handler for inverse toggle, deleted unused script

---
+++ Assets/Meshing/Scripts/KeyUIHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class KeyUIHandler : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_UI;
+
+    public GameObject ui
+    {
+        get => m_UI;
+        set => m_UI = value;
+    }
+
+    public void ToggleVisability()
+    {
+        m_UI.SetActive(!m_UI.activeSelf);
+    }
+}
