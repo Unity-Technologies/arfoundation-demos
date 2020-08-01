6ec8caa9c509f6e00bf52cefc5baa4e46d4a712e
Arturo Nereu <hdgam3r@gmail.com>
2020-08-01 12:48:50 -0500

    Added Wireframe shader base

---
+++ Assets/Shaders/Scripts/BarycentricDataBuilder.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class BarycentricDataBuilder : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
