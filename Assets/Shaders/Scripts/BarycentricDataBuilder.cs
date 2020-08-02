cc29bca80651aaf7b31c598d41224981fda8842f
Arturo Nereu <hdgam3r@gmail.com>
2020-08-02 12:10:55 -0500

    Commented out unused UV data from mesh.
    Refactored Shader Graph nodes.

--- Assets/Shaders/Scripts/BarycentricDataBuilder.cs
+++ Assets/Shaders/Scripts/BarycentricDataBuilder.cs
@@ -7,24 +7,27 @@ public class BarycentricDataBuilder : MonoBehaviour
 {
     private Mesh m_Mesh;
 
-    // Start is called before the first frame update
     void Start()
     {
         GenerateBarycentricData();
     }
 
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    GenerateBarycentricData();
-    //}
+    private void Reset()
+    {
+        GenerateBarycentricData();
+    }
 
     void GenerateBarycentricData()
     {
-        m_Mesh = GetComponent<MeshFilter>().mesh;
+        m_Mesh = GetComponent<MeshFilter>().sharedMesh;
 
         SplitMesh(m_Mesh);
 
+        SetVertexColors(m_Mesh);
+    }
+
+    void SetVertexColors(Mesh mesh)
+    {
         Color[] colorCoords = new[]
         {
             new Color(1, 0, 0),
@@ -34,13 +37,6 @@ public class BarycentricDataBuilder : MonoBehaviour
 
         Color32[] vertexColors = new Color32[m_Mesh.vertexCount];
 
-        //compute vertexcolors
-        //for(int i = 0; i < vertexColors.Length; i++)
-        //{
-        //    vertexColors[i] = colorCoords[(int)Mathf.Repeat(i, 3)];
-        //    //vertexColors[i] = Color.red;
-        //}
-
         for (int i = 0; i < vertexColors.Length; i += 3)
         {
             vertexColors[i] = colorCoords[0];
@@ -48,21 +44,7 @@ public class BarycentricDataBuilder : MonoBehaviour
             vertexColors[i + 2] = colorCoords[2];
         }
 
-        //for (int i = 0; i < m_Mesh.triangles.Length; i++)
-        //{
-        //    vertexColors[m_Mesh.triangles[i]] = colorCoords[(int)Mathf.Repeat(i, 3)];
-
-        //    //vertexColors[m_Mesh.triangles[i]] = colorCoords[0];
-        //    //vertexColors[m_Mesh.triangles[i + 1]] = colorCoords[1];
-        //    //vertexColors[m_Mesh.triangles[i + 2]] = colorCoords[2];
-        //}
-
-        //compute vertexcolors
-
         m_Mesh.colors32 = vertexColors;
-
-        //m_Mesh.RecalculateNormals();
-        //m_Mesh.MarkModified();
     }
 
     void SplitMesh(Mesh mesh)
@@ -70,30 +52,30 @@ public class BarycentricDataBuilder : MonoBehaviour
         int[] triangles = mesh.triangles;
         Vector3[] verts = mesh.vertices;
         Vector3[] normals = mesh.normals;
-        Vector2[] uvs = mesh.uv;
+        //Vector2[] uvs = mesh.uv;
 
         Vector3[] newVerts;
         Vector3[] newNormals;
-        Vector2[] newUvs;
+        //Vector2[] newUvs;
 
         int n = triangles.Length;
         newVerts = new Vector3[n];
         newNormals = new Vector3[n];
-        newUvs = new Vector2[n];
+        //newUvs = new Vector2[n];
 
         for (int i = 0; i < n; i++)
         {
             newVerts[i] = verts[triangles[i]];
             newNormals[i] = normals[triangles[i]];
-            if (uvs.Length > 0)
-            {
-                newUvs[i] = uvs[triangles[i]];
-            }
+            //if (uvs.Length > 0)
+            //{
+            //    newUvs[i] = uvs[triangles[i]];
+            //}
             triangles[i] = i;
         }
         mesh.vertices = newVerts;
         mesh.normals = newNormals;
-        mesh.uv = newUvs;
+        //mesh.uv = newUvs;
         mesh.triangles = triangles;
     }
 }
