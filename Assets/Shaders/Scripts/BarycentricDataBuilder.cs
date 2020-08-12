45a3bc3989d141992a4dd47dee2df32c42f9c0d8
Arturo Nereu <hdgam3r@gmail.com>
2020-08-12 17:12:34 -0500

    Circular based rendering added

--- Assets/Shaders/Scripts/BarycentricDataBuilder.cs
+++ Assets/Shaders/Scripts/BarycentricDataBuilder.cs
@@ -1,14 +1,15 @@
 ﻿// Based on: https://catlikecoding.com/unity/tutorials/advanced-rendering/flat-and-wireframe-shading/
-
+using System.Collections.Generic;
 using UnityEngine;
 
 //[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class BarycentricDataBuilder : MonoBehaviour
 {
-    //void Start()
-    //{
-    //    GenerateBarycentricData();
-    //}
+    void Start()
+    {
+        //GenerateBarycentricData();
+        GenerateUVData();
+    }
 
     private void Reset()
     {
@@ -22,6 +23,12 @@ public class BarycentricDataBuilder : MonoBehaviour
         SplitMesh(mesh);
 
         SetVertexColors(mesh);
+    }
+
+    void GenerateUVData()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+        GenerateUVs(mesh);
     }
 
     public void GenerateData(Mesh mesh)
@@ -83,5 +90,52 @@ public class BarycentricDataBuilder : MonoBehaviour
         mesh.normals = newNormals;
         mesh.uv = newUvs;
         mesh.triangles = triangles;
+    }
+
+    static List<Vector3> s_Vertices = new List<Vector3>();
+    static List<Vector3> s_UVs = new List<Vector3>();
+    [SerializeField]
+    private float m_VertexDistance;
+
+    public void GenerateUVs(Mesh mesh)
+    {
+        int vertexCount = mesh.vertexCount;
+
+        s_UVs.Clear();
+        if (s_UVs.Capacity < vertexCount) { s_UVs.Capacity = vertexCount; }
+
+        mesh.GetVertices(s_Vertices);
+
+        Vector3 centerInPlaneSpace = s_Vertices[s_Vertices.Count - 1];
+        Vector3 uv = new Vector3(0, 0, 0);
+        float shortestUVMapping = float.MaxValue;
+
+        // Assume the last vertex is the center vertex.
+        for (int i = 0; i < vertexCount - 1; i++)
+        {
+            float vertexDist = Vector3.Distance(s_Vertices[i], centerInPlaneSpace);
+
+            // Remap the UV so that a UV of "1" marks the feathering boudary.
+            // The ratio of featherBoundaryDistance/edgeDistance is the same as featherUV/edgeUV.
+            // Rearrange to get the edge UV.
+            float uvMapping = vertexDist / Mathf.Max(vertexDist-0.2f, 0.001f);
+            uv.x = vertexDist/uvMapping;
+
+            // All the UV mappings will be different. In the shader we need to know the UV value we need to fade out by.
+            // Choose the shortest UV to guarentee we fade out before the border.
+            // This means the feathering widths will be slightly different, we again rely on a fairly uniform plane.
+            //if (shortestUVMapping > uvMapping) 
+            //{ 
+            //    shortestUVMapping = uvMapping; 
+            //}
+
+            s_UVs.Add(uv);
+        }
+
+        uv.Set(0, 0, 0);
+        s_UVs.Add(uv);
+
+        mesh.SetUVs(0, s_UVs);
+        mesh.UploadMeshData(false);
     }
 }
---
+++ Assets/Shaders/Scripts/ContactPosition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactPosition : MonoBehaviour
+{
+    private Material m_TargetMaterial;
+    private int m_PropertyHash;
+
+    public GameObject m_TargetObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_PropertyHash = Shader.PropertyToID("_ContactPosition");
+        m_TargetMaterial = m_TargetObject.GetComponent<Renderer>().sharedMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_TargetMaterial.SetVector(m_PropertyHash, transform.position);   
+    }
+}
