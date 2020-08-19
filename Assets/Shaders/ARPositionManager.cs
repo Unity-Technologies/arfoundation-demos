8b8002fa20c89e69041a2a6bf567da4fe93be9d3
Dan <danielmi@unity3d.com>
2020-08-18 23:56:38 -0700

    wip delayed position placement

--- Assets/Shaders/ARPositionManager.cs
+++ Assets/Shaders/ARPositionManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
@@ -12,6 +13,13 @@ public class ARPositionManager : MonoBehaviour
     [SerializeField]
     ARPlaneManager m_PlaneManager;
 
+    [SerializeField]
+    GameObject m_ObjectPrefab;
+
+    GameObject m_SpawnedObject;
+    Vector3 m_DelayedPosition;
+    bool m_Placed = false;
+
     TrackableId m_HitID;
     Pose m_HitPose;
 
@@ -20,6 +28,12 @@ public class ARPositionManager : MonoBehaviour
     const string k_ShaderPositionProperty = "_ContactPosition";
     const string k_ShaderContactProperty = "_IsInContact";
 
+    void OnEnable()
+    {
+        m_SpawnedObject = Instantiate(m_ObjectPrefab);
+        m_DelayedPosition = new Vector3(0,0,0);
+    }
+    
     void Update()
     {
         if (m_RaycastManager.Raycast(CenterScreenHelper.Instance.GetCenterScreen(), s_Hits, TrackableType.PlaneWithinBounds))
@@ -27,17 +41,35 @@ public class ARPositionManager : MonoBehaviour
             m_HitID = s_Hits[0].trackableId;
             m_HitPose = s_Hits[0].pose;
         }
-        
+
+        m_DelayedPosition = Vector3.Lerp(m_DelayedPosition, m_HitPose.position, 0.085f);
+
+        if (!m_Placed)
+        {
+            m_SpawnedObject.transform.position = m_DelayedPosition;
+        }
+
         foreach(ARPlane plane in m_PlaneManager.trackables)
         {
             if (plane.trackableId == m_HitID)
             {
                 plane.transform.GetComponent<MeshRenderer>().material.SetFloat(k_ShaderContactProperty, 1);
-                plane.transform.GetComponent<MeshRenderer>().material.SetVector(k_ShaderPositionProperty, m_HitPose.position);
+                plane.transform.GetComponent<MeshRenderer>().material.SetVector(k_ShaderPositionProperty, m_DelayedPosition);
             }
             else
             {
                 plane.transform.GetComponent<MeshRenderer>().material.SetFloat(k_ShaderContactProperty, 0);
+            }
+        }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                m_Placed = true;
+                m_SpawnedObject.GetComponent<AnimatedPlaceObject>().AnimatePlacement();
             }
         }
     }
---
+++ Assets/Shaders/AnimatedPlaceObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.XR.ARSubsystems;
+
+public class AnimatedPlaceObject : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform m_PlacementSphere;
+
+    public float ShakeVal;
+
+    public Ease MoveEase;
+
+    void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            m_PlacementSphere.localPosition = new Vector3(0, 0.375f, 0);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AnimatePlacement();
+        }
+#endif
+    }
+
+    public void AnimatePlacement()
+    {
+        m_PlacementSphere.DOLocalMove(Vector3.zero, 0.1f).SetEase(MoveEase).OnComplete(BounceLand);
+    }
+
+    void BounceLand()
+    {
+        m_PlacementSphere.DOShakeScale(0.1f, ShakeVal);
+    }
+}
